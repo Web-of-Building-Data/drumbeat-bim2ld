bbe6c6854612764bb6a341b009e55e3209de0a85
Nam Vu Hoang <nam.vuhoang@aalto.fi>
2017-02-03 01:24:50 +0200

    * Delete drumbeat2-data.bedm

--- drumbeat2-data.bedm/src/main/java/fi/aalto/cs/drumbeat/data/bedm/schema/BemSimpleNonCollectionTypeInfo.java
+++
@@ -1,9 +0,0 @@
-package fi.aalto.cs.drumbeat.data.bem.schema;
-
-public abstract class BemSimpleNonCollectionTypeInfo extends BemNonCollectionTypeInfo {
-
-	public BemSimpleNonCollectionTypeInfo(BemSchema schema, String name) {
-		super(schema, name);
-	}
-
-}
